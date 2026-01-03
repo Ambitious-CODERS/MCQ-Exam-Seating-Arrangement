fa18886af182816548a56172d37409997cf5c85b
Om Mankar <ommankar2023@gmail.com>
2026-01-03 18:28:12 +0530

    MCQ

---
+++ MCQ-Examination-main/config.php
@@ -0,0 +1,13 @@
+<?php
+    $server_name = "localhost";
+    $username = "root";
+    $password = ""; // or your actual password
+    $database_name = "mcq";
+    $port = 3307;
+
+    $conn = mysqli_connect($server_name, $username, $password, $database_name, $port);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
