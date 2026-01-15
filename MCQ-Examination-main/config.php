51c83d1e098f7c373d97259d944d689a84bcb02a
Om Mankar <ommankar2023@gmail.com>
2026-01-15 13:34:07 +0530

    Update config.php

--- MCQ-Examination-main/config.php
+++ MCQ-Examination-main/config.php
@@ -2,7 +2,7 @@
     $server_name = "localhost";
     $username = "root";
     $password = ""; // or your actual password
-    $database_name = "mcq";
+    $database_name = "db1";
     $port = 3307;
 
     $conn = mysqli_connect($server_name, $username, $password, $database_name, $port);
